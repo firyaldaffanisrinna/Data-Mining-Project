843220b8aef963b5d4237c471ff845dec2fd9332
firyaldaffanisrinna <firyaldaffanisrinna@students.unnes.ac.id>
2025-05-28 19:59:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Brain Tumor Dataset App", layout="wide")
-st.title("🧠 Brain Tumor Dataset Dashboard")
+st.set_page_config(page_title="Avocado Ripness Dataset App", layout="wide")
+st.title("🥑 Avocado Ripness Dashboard")
 st.sidebar.success("Pilih halaman")
 
